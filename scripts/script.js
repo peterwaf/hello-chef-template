2475e2ee3c1726dd238ac8faee0395122a75346e
peterwaf <peterwafulah@gmail.com>
2024-03-04 19:25:15 +0300

    create mobile menu

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+var visibleMobileMenuAndIcon = document.getElementById("visible_mobile_logo_and_icon");
+var popUpMobileMenu = document.getElementById("logo_menu_pop_up");
+var visibleHumburgerIcon = document.getElementById("humbeger");
+var mobilePopUpMenu = document.getElementById("logo_menu_pop_up");
+var mobilePopUpX_Icon = document.getElementById("humberger_menu_poped");
+visibleHumburgerIcon.onclick = function () {
+    mobilePopUpMenu.style.display = "block";
+
+}
+mobilePopUpX_Icon.onclick = function () {
+    mobilePopUpMenu.style.display = "none";
+}
