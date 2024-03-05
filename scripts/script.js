6efdee90ab1d5375413b3ec81602478da79fb0a1
peterwaf <peterwafulah@gmail.com>
2024-03-05 11:01:12 +0300

    fix visible mobile menu bg

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,9 @@ var popUpMobileMenu = document.getElementById("logo_menu_pop_up");
 var visibleHumburgerIcon = document.getElementById("humbeger");
 var mobilePopUpMenu = document.getElementById("logo_menu_pop_up");
 var mobilePopUpX_Icon = document.getElementById("humberger_menu_poped");
+mobilePopUpMenu.style.display = "none";
 visibleHumburgerIcon.onclick = function () {
     mobilePopUpMenu.style.display = "block";
-
 }
 mobilePopUpX_Icon.onclick = function () {
     mobilePopUpMenu.style.display = "none";
